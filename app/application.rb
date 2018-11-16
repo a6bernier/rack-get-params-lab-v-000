b4349d0b8063f97ad5eed90d1e819d335decb19b
Andrew Bernier <a6bernier@gmail.com>
2018-11-16 19:07:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,7 @@ class Application
 
     if @@items.include?(add_item)
       resp.write "added #{add_item}"
+      @@cart << add_item
     else
       resp.write "We don't have that item."
     end
